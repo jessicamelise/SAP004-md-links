196fd058e19906b2633f6f8e05a7481efc559f27
jessicamelise <jessica.melise@hotmail.com>
2020-07-04 02:02:22 -0300

    aplicado leitura de diretório na função mdlinks

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 const fs = require('fs');
+const path = require('path');
+const { resolve } = require('path');
 
 const mdLinks = (file) => {
-  const promiseLinks = new Promise((res, rej) => {
+  return new Promise((resolve, reject) => {
     let arrayLinks = [];
     fs.stat(file, (errFile, stats) => {
-      if (!errFile) {
-        if (stats.isFile()) {
-          fs.readFile(file, 'utf8', (errReadFile, data) => {
-            if (!errReadFile) {
-              const regexMdLinks = /\[([^\]]*)\]\(([^\)]*)\)/gm;
-              let firstFilter = data.match(regexMdLinks);
-              firstFilter.forEach((textAndLink) => {
-                let verifyBreakLine = textAndLink.replace(/(\r\n|\n|\r)/gm, '')
-                const text = verifyBreakLine.match(/\[([^\]]*)\]/)[1];
-                const href = verifyBreakLine.match(/\(([^\)]*)\)/)[1];
-                arrayLinks.push({ text, href, file });
-              })
-              // console.log(arrayLinks);
-              res(arrayLinks);
-            } else {
-              rej(errReadFile.message);
-            }
-          });
-        } else if (stats.isDirectory()) {
-          console.log('is directory? ' + stats.isDirectory());
-          fs.readdir(fileOrDir, 'utf-8', (err, file) => {
-            console.log(fileOrDir);
-            console.log(err);
-            console.log(file);
-          });
-        }
+      if (errFile) {
+        reject(errFile.message);
+      } else if (stats.isDirectory()) {
+        fs.readdir(file, 'utf-8', (errDir, files) => {
+          if (!errDir) {
+            files.forEach((archive) => {
+              const verifyDot = archive.match(/^\w[^\s]*/);
+              if (verifyDot) {
+                mdLinks(`${file}\\${verifyDot[0]}`)
+              }
+            })
+          } else {
+            console.log(errDir.message)
+          }
+        });
+      } else {
+        fs.readFile(file, 'utf8', (errReadFile, data) => {
+          if (errReadFile) {
+            console.log(errReadFile.message);
+          } else if (path.extname(file) === '.md') {
+            const regexMdLinks = /\[([^\]]*)\]\(([^\)]*)\)/gm;
+            let firstFilter = data.match(regexMdLinks);
+            firstFilter.forEach((textAndLink) => {
+              let verifyBreakLine = textAndLink.replace(/(\r\n|\n|\r)/, '')
+              const text = verifyBreakLine.match(/\[([^\]]*)\]/)[1];
+              const href = verifyBreakLine.match(/\]\(([^\)]*)\)/)[1];
+              arrayLinks.push({ text, href, file });
+            });
+            // console.log(arrayLinks);
+            console.log(resolve(arrayLinks))
+            return arrayLinks
+          }
+        });
       }
-      else
-        rej(errFile.message);
     });
   });
-  // console.log(promiseLinks);
-  return promiseLinks;
 }
 
 // module.exports = mdLinks
-mdLinks('c:\\Users\\jessi\\Documents\\Programacao\\Javascript\\Laboratoria efetivo\\SAP004-md-links\\README.md')
+mdLinks('c:\\Users\\jessi\\Documents\\Programacao\\Javascript\\Laboratoria efetivo\\SAP004-md-links')
