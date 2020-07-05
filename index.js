f2e6c3c4737acf4b6624559cc348da368d5f5d94
jessicamelise <jessica.melise@hotmail.com>
2020-07-05 14:36:36 -0300

    refatoração das funções que envolvem a opção de validação = true

--- index.js
+++ index.js
@@ -4,43 +4,40 @@ const http = require('http');
 const https = require('https');
 
 const mdLinks = (file, options) => {
-  // let o = {
-  //   validade: false,
-  //   stats: false,
-  // };
-  // Object.assign(o,options)
+  let op = { validade: false, stats: false };
+  Object.assign(op, options);
   return new Promise((resolve, reject) => {
     fs.stat(file, (errFile, stats) => {
       if (errFile) {
         reject(errFile.message);
       } else if (stats.isDirectory()) {
-        resolveDirectory(file, options, resolve, reject);
+        resolveDirectory(file, op, resolve, reject);
       } else {
-        resolveFile(file, options, resolve, reject);
+        resolveFile(file, op, resolve, reject);
       }
     });
   });
 }
 
-const resolveDirectory = (file, options, resolve, reject) => {
+const resolveDirectory = (file, op, resolve, reject) => {
   fs.readdir(file, 'utf-8', (errDir, files) => {
     if (!errDir) {
       let arrayMdlinks = files.map((archive) => {
-        return mdLinks(`${file}\\${archive}`, options);
+        return mdLinks(`${file}\\${archive}`, op);
       })
       Promise.all(arrayMdlinks).then((objectList) => {
         const list = objectList.flat();
         resolve(list);
       })
     } else {
-      reject(errDir.message)
+      reject(errDir.message);
     }
   });
 }
 
 const regexMdLinks = /\[([^\]]*)\]\(([^\)]*)\)/gm;
 
-const resolveFile = (file, options, resolve, reject) => {
+const resolveFile = (file, op, resolve, reject) => {
   fs.readFile(file, 'utf8', (errReadFile, data) => {
     let arrayLinks = [];
     if (errReadFile) {
@@ -48,19 +45,9 @@ const resolveFile = (file, options, resolve, reject) => {
     } else if (path.extname(file) === '.md') {
       let filterRegex = data.match(regexMdLinks);
       if (filterRegex) {
-        filterRegex.forEach((textAndLink) => {
-          let verifyBreakLine = textAndLink.replace(/(\r\n|\n|\r)/, '')
-          const text = verifyBreakLine.match(/\[([^\]]*)\]/)[1];
-          const href = verifyBreakLine.match(/\]\(([^\)]*)\)/)[1];
-          arrayLinks.push({ text, href, file });
-        });
-        if (options && options.validate) {
-          let teste = arrayLinks.map((item) => {
-            return validateLink(item)
-          })
-          Promise.all(teste).then((t) => {
-            resolve(t);
-          })
+        creatingArrayOfLinksInformation(filterRegex, arrayLinks, file);
+        if (op.validate) {
+          optionValidate(arrayLinks, resolve);
           return;
         }
       }
@@ -69,25 +56,38 @@ const resolveFile = (file, options, resolve, reject) => {
   });
 }
 
+const creatingArrayOfLinksInformation = (filteredValue, array, file) => {
+  filteredValue.forEach((textAndLink) => {
+    let verifyBreakLine = textAndLink.replace(/(\r\n|\n|\r)/, '');
+    const text = verifyBreakLine.match(/\[([^\]]*)\]/)[1];
+    const href = verifyBreakLine.match(/\]\(([^\)]*)\)/)[1];
+    array.push({ text, href, file });
+  });
+}
+
+const optionValidate = (array, resolve) => {
+  let arrayObjOfLinks = array.map((item) => {
+    return validateLink(item);
+  });
+  Promise.all(arrayObjOfLinks).then((resObj) => {
+    resolve(resObj);
+  });
+}
+
 const validateLink = (objLink) => {
   return new Promise((resolve) => {
     if (objLink.href.indexOf('https') === 0) {
       https.get(objLink.href, (res) => {
-        const code = res.statusCode;
-        const message = res.statusMessage;
-        objLink.validate = { code, message };
+        objLink.validate = { code: res.statusCode, message: res.statusMessage };
         resolve(objLink);
-      })
+      });
     } else if (objLink.href.indexOf('http') === 0) {
       http.get(objLink.href, (res) => {
-        const code = res.statusCode;
-        const message = res.statusMessage;
-        objLink.validate = { code, message };
+        objLink.validate = { code: res.statusCode, message: res.statusMessage };
         resolve(objLink);
-      })
+      });
     } else {
-      const message = 'invalid format';
-      objLink.validate = { message };
+      objLink.validate = { message: 'invalid format' };
       resolve(objLink);
     }
   })
