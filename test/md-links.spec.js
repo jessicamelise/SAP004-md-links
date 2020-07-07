42505cee48f8e1f52e9fa3d20b553b4178b48214
jessicamelise <jessica.melise@hotmail.com>
2020-07-07 19:20:44 -0300

    criação de testes para o resolve da promise retornada pela mdLinks

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,49 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  test('should be a function', () => {
+    expect(typeof mdLinks).toBe('function');
   });
 
+  test('should return all links of the directory', () => {
+    return mdLinks('./test/dir-test').then((links) => {
+      const result = links.length;
+      expect(result).toEqual(7);
+    });
+  });
+
+  test('should return error for wrong files', () => {
+    expect.assertions(1);
+    return mdLinks('/lala').catch((error) => {
+      expect(error).toMatch("ENOENT: no such file or directory, stat 'C:\\lala'");
+    });
+  });
+
+  test('should return all links with validation', () => {
+    return mdLinks('./test/dir-test', { validate: true }).then((links) => {
+      const result = links.map((eachLink) => {
+        return eachLink.validate;
+      })
+      expect(result.length).toEqual(7);
+    });
+  });
+
+  ///falta teste do validate erro .catch
+
+  test('should return the statistic of total number of links and non-repeated links', () => {
+    return mdLinks('./test/dir-test', { stats: true }).then((links) => {
+      expect(links).toEqual({ Total: 7, Unique: 6 });
+    });
+  });
+
+  ///falta teste do stats erro .catch
+
+  test('should return the statistic of total number of links, non-repeated links and broken links', () => {
+    return mdLinks('./test/dir-test', { stats: true, validate:true }).then((links) => {
+      expect(links).toEqual({ Total: 7, Unique: 6, Broken: 3 });
+    });
+  });
+
+    ///falta teste do stats erro .catch
 });
