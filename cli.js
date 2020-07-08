96ebe74dfd8dd92d1a20c04a8b5be3bedcc078c5
jessicamelise <jessica.melise@hotmail.com>
2020-07-08 20:12:42 -0300

    criando arquivo cli.js

---
+++ cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const mdLinks = require("./index.js");
+
+const file = process.argv[2];
+console.log(file)
+
+const validate = process.argv.includes('--validate')
+const stats = process.argv.includes('--stats');
+
+mdLinks(file, {validate, stats}).then((result) => {
+    console.log(result);
+});
