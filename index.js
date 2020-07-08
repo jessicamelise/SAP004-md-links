1980348874708124aa99afb69500672d74b4d670
jessicamelise <jessica.melise@hotmail.com>
2020-07-07 21:17:33 -0300

    ajustando valores dos testes conforme novo link de teste adicionado no arquivo .md de teste

--- index.js
+++ index.js
@@ -10,16 +10,14 @@ const mdLinks = (file, options) => {
     try {
       getAllLinks(file, op).then((allLinks) => {
         if (op.stats) {
-          statsWithOrWithoutValidate(allLinks, op, resolve);
+          statsWithOrWithoutValidate(allLinks, op, resolve, reject);
         } else if (op.validate) {
-          optionValidate(allLinks, resolve);
+          optionValidate(allLinks, resolve, reject);
         } else {
           resolve(allLinks);
         }
-      });
-    } catch (err) {
-      reject(err);
-    }
+      }).catch (e => reject(e));
+    } catch (err) { reject(err) };
   });
 }
 
@@ -39,17 +37,19 @@ const getAllLinks = (file, op) => {
 
 const resolveDirectory = (file, op, resolve, reject) => {
   fs.readdir(file, 'utf-8', (errDir, files) => {
-    if (!errDir) {
-      let arrayMdlinks = files.map((archive) => {
-        return getAllLinks(`${file}/${archive}`, op);
-      })
-      Promise.all(arrayMdlinks).then((objectList) => {
-        const list = objectList.flat();
-        resolve(list);
-      })
-    } else {
-      reject(errDir.message);
-    }
+    try {
+      if (!errDir) {
+        let arrayMdlinks = files.map((archive) => {
+          return getAllLinks(`${file}/${archive}`, op);
+        });
+        Promise.all(arrayMdlinks).then((objectList) => {
+          const list = objectList.flat();
+          resolve(list);
+        }).catch(e=>reject(e));
+      } else {
+        reject(errDir.message);
+      }
+    } catch (err) { reject(err) };
   });
 }
 
@@ -57,7 +57,8 @@ const regexMdLinks = /\[([^\]]*)\]\(([^\)]*)\)/gm;
 
 const resolveFile = (file, resolve, reject) => {
   fs.readFile(file, 'utf8', (errReadFile, data) => {
-    let arrayLinks = [];
+    try {
+      let arrayLinks = [];
     if (errReadFile) {
       reject(errReadFile.message);
     } else if (path.extname(file) === '.md') {
@@ -67,6 +68,7 @@ const resolveFile = (file, resolve, reject) => {
       }
     }
     resolve(arrayLinks);
+    } catch (err) { reject(err) };
   });
 }
 
@@ -79,38 +81,40 @@ const creatingArrayOfLinksInformation = (filteredValue, array, file) => {
   });
 }
 
-const optionValidate = (array, resolve) => {
+const optionValidate = (array, resolve, reject) => {
   let arrayObjOfLinks = array.map((item) => {
     return validateLink(item);
   });
   Promise.all(arrayObjOfLinks).then((resObj) => {
     resolve(resObj);
-  });
+  }).catch(e=>reject(e));
 }
 
 const validateLink = (objLink) => {
-  return new Promise((resolve) => {
-    if (objLink.href.indexOf('https') === 0) {
-      https.get(objLink.href, (res) => {
-        objLink.validate = { code: res.statusCode, message: res.statusMessage };
+  return new Promise((resolve, reject) => {
+    try {
+      if (objLink.href.indexOf('https:') === 0) {
+        https.get(objLink.href, (res) => {
+          objLink.validate = { code: res.statusCode, message: res.statusMessage };
+          resolve(objLink);
+        }).on('error', (err) => {
+          objLink.validate = (`Got error: ${err.code}`);
+          resolve(objLink);
+        });
+      } else if (objLink.href.indexOf('http:') === 0) {
+        http.get(objLink.href, (res) => {
+          objLink.validate = { code: res.statusCode, message: res.statusMessage };
+          resolve(objLink);
+        }).on('error', (err) => {
+          objLink.validate = (`Got error: ${err.code}`);
+          resolve(objLink);
+        });
+      } else {
+        objLink.validate = { message: 'invalid format' };
         resolve(objLink);
-      }).on('error', (err) => {
-        objLink.validate = (`Got error: ${err.code}`);
-        resolve(objLink);
-      });
-    } else if (objLink.href.indexOf('http') === 0) {
-      http.get(objLink.href, (res) => {
-        objLink.validate = { code: res.statusCode, message: res.statusMessage };
-        resolve(objLink);
-      }).on('error', (err) => {
-        objLink.validate = (`Got error: ${err.code}`);
-        resolve(objLink);
-      });
-    } else {
-      objLink.validate = { message: 'invalid format' };
-      resolve(objLink);
-    }
-  })
+      }
+    } catch (err) { reject(err) };
+  });
 }
 
 const optionStats = (arrayWithAllLinks, addValidate) => {
@@ -140,14 +144,14 @@ const findDuplicateLinks = (arrayLinks) => {
   return result;
 }
 
-const statsWithOrWithoutValidate = (arrayWithAllLinks, op, resolve) => {
+const statsWithOrWithoutValidate = (arrayWithAllLinks, op, resolve, reject) => {
   if (op.validate) {
-    return optionValidadeWithStats(arrayWithAllLinks, resolve);
+    return optionValidateWithStats(arrayWithAllLinks, resolve, reject);
   }
   resolve(optionStats(arrayWithAllLinks))
 }
 
-const optionValidadeWithStats = (array, resolve) => {
+const optionValidateWithStats = (array, resolve, reject) => {
   let totalWithStatsAndValidate = '';
   let resultArray = array.map((eachObjLink) => {
     return validateLink(eachObjLink);
@@ -162,12 +166,12 @@ const optionValidadeWithStats = (array, resolve) => {
     });
     totalWithStatsAndValidate = optionStats(array, brokenCodes.length)
     resolve(totalWithStatsAndValidate);
-  });
+  }).catch(e=> reject(e));
 }
 
-module.exports = mdLinks;
-
-// mdLinks('C:/Users/jessi/Documents/Programacao/Javascript/Testes/teste-controlado', {stats:true, validate:true})
+// mdLinks('/lala')
 //   .then((links) => {
 //     console.log(links);
-//   })
+//   }).catch(err=>console.log(err))
+
+module.exports = mdLinks;
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -9,7 +9,7 @@ describe('mdLinks', () => {
   test('should return all links of the directory', () => {
     return mdLinks('./test/dir-test').then((links) => {
       const result = links.length;
-      expect(result).toEqual(7);
+      expect(result).toEqual(8);
     });
   });
 
@@ -25,7 +25,7 @@ describe('mdLinks', () => {
       const result = links.map((eachLink) => {
         return eachLink.validate;
       })
-      expect(result.length).toEqual(7);
+      expect(result.length).toEqual(8);
     });
   });
 
@@ -33,7 +33,7 @@ describe('mdLinks', () => {
 
   test('should return the statistic of total number of links and non-repeated links', () => {
     return mdLinks('./test/dir-test', { stats: true }).then((links) => {
-      expect(links).toEqual({ Total: 7, Unique: 6 });
+      expect(links).toEqual({ Total: 8, Unique: 7 });
     });
   });
 
@@ -41,7 +41,7 @@ describe('mdLinks', () => {
 
   test('should return the statistic of total number of links, non-repeated links and broken links', () => {
     return mdLinks('./test/dir-test', { stats: true, validate:true }).then((links) => {
-      expect(links).toEqual({ Total: 7, Unique: 6, Broken: 3 });
+      expect(links).toEqual({ Total: 8, Unique: 7, Broken: 4 });
     });
   });
 
